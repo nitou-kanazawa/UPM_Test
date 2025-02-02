28869552d48f2a9551fb34f046ec263e5520622f
nitou-kanazawa <121334451+nitou-kanazawa@users.noreply.github.com>
2025-02-02 20:23:15 +0900

    SettingsProvider関連のボイラープリントを別リポジトリに移動．

--- Assets/com.nitou.nModules/Core Modules/Custom Settings/Editor/SettingsProviderKey.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Nitou.EditorShared{
-
-    internal static class SettingsProviderKey{
-
-        public const string Preference = "Preferences/_Nitou/";
-        public const string ProjectSettings = "Project/Nitou Modules/";
-    }
-}
